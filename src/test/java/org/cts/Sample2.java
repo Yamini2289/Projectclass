c87f41df91c59020fb5af02793e956780ba6edcb
Yamini <myamini2010@gmail.com>
2023-02-10 08:58:34 +0530

    code for login functionality

---
+++ src/test/java/org/cts/Sample2.java
@@ -0,0 +1,19 @@
+package org.cts;
+
+public class Sample2 {
+
+private void medhod1() {
+	System.out.println("Yamini");
+
+}
+		
+private void medhod2() {
+	System.out.println("Hari");
+
+}
+
+private void medhod3() {
+	System.out.println("DD");
+
+}
+}
